5196294919a0c80d49c0e935b0103c9044d17ecc
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-09-01 16:52:30 +0800

    验证二叉查找树

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -5075,19 +5075,19 @@ public class Solutions {
         else if (cur.left != null && cur.right == null) {
             if (cur.val > cur.left.val) {
                 CheckBSTResult temp = checkBST(cur.left);
-                return new CheckBSTResult(temp.maxVal < cur.val, temp.minVal, cur.val);
+                return new CheckBSTResult(temp.isBST && temp.maxVal < cur.val, temp.minVal, cur.val);
             }
         }
         else if (cur.left == null && cur.right != null) {
             if (cur.val < cur.right.val) {
                 CheckBSTResult temp = checkBST(cur.right);
-                return new CheckBSTResult(temp.minVal > cur.val, cur.val, temp.maxVal);
+                return new CheckBSTResult(temp.isBST && temp.minVal > cur.val, cur.val, temp.maxVal);
             }
         }
         else {
             CheckBSTResult tempL = checkBST(cur.left);
             CheckBSTResult tempR = checkBST(cur.right);
-            if (tempL.maxVal < cur.val && tempR.minVal > cur.val) {
+            if (tempL.isBST && tempR.isBST && tempL.maxVal < cur.val && tempR.minVal > cur.val) {
                 return new CheckBSTResult(true, tempL.minVal, tempR.maxVal);
             }
         }
@@ -5130,8 +5130,6 @@ public class Solutions {
          上述这棵二叉树序列化为 {2,1,4,#,#,3,5}.
          */
 //        TreeNode root = TreeNodeFactory.build("2,1,4,#,#,3,5");
-        TreeNode root = TreeNodeFactory.build("989,982,#,972,#,947,#,920,#,903,#,894,#,881,#,866,#,864,#,842,#,841,#,796,#,726,#,647,#,613,719,593,#,#,#,590,#,558,#,554,#,538,#,512,#,504,#,468,505,467,#,#,#,456,#,413,#,331,#,330,407,320,#,#,#,312,#,306,#,301,#,274,#,251,#,235,#,231,#,222,#,181,#,93,#,83,#,73,#,64,#,62,#,60,#,28,#,21,#,20,#,-32,#,-52,#,-70,#,-87,#,-98,#,-102,#,-115,#,-116,#,-139,#,-183,#,-224,#,-241,#,-263,#,-284,#,-294,#,-296,#,-320,#,-330,#,-392,#,-398,#,-407,#,-431,#,-445,#,-460,#,-463,#,-492,#,-507,#,-518,#,-539,#,-552,#,-558,#,559,#,-587,#,-673,#,-736,#,-757,#,-766,#,-767,#,-823,#,-830,#,-867,#,-875,#,-891,#,-905,#,-910,#,-924,#,-960,#,-985,#,-988");
-        XYLog.d(solutions.isValidBST(root));
 //        XYLog.d(root, solutions.isValidBST(root)? "是": "不是", "二叉查找树");
 
 
