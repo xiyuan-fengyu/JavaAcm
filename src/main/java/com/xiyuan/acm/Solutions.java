e47af136355d09fe7e069c46dc290fb26a6e1a0c
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-09-05 18:30:03 +0800

    XYLog bug 修复

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -5732,8 +5732,52 @@ public class Solutions {
 
 
 
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/triangle/
+     * @param triangle: a list of lists of integers.
+     * @return: An integer, minimum path sum.
+     */
+    public int minimumTotal(int[][] triangle) {
+
+        return 0;
+    }
+
+
+
+
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
+
+        /**
+         数字三角形
+         给定一个数字三角形，找到从顶部到底部的最小路径和。每一步可以移动到下面一行的相邻数字上。
+         注意事项
+         如果你只用额外空间复杂度O(n)的条件下完成可以获得加分，其中n是数字三角形的总行数。
+
+         样例
+         比如，给出下列数字三角形：
+         [
+             [2],
+            [3,4],
+          [6,5,7],
+         [4,1,8,3]
+         ]
+         从顶到底部的最小路径和为11 ( 2 + 3 + 5 + 1 = 11)。
+         */
+        int[][] triangle = {
+                {2},
+                {3,4},
+                {6,5,7},
+                {4,1,8,3}
+        };
+        XYLog.d(triangle);
+
+
+
 
         /**
          分割回文串 II
