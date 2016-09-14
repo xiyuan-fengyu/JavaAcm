b78837dbb68c0f08c9367339cca9d6b587a0d4b1
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-09-14 11:54:12 +0800

    单词接龙   [中等]

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -7481,8 +7481,118 @@ public class Solutions {
     }
 
 
+
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/word-ladder/
+     * @param start, a string
+     * @param end, a string
+     * @param dict, a set of string
+     * @return an integer
+     */
+    public int ladderLength(String start, String end, Set<String> dict) {
+        if (start != null && end != null) {
+            if (start.equals(end)) {
+                return 1;
+            }
+            else if (dict.contains(end) && canStrTranslate(start, end)) {
+                return 2;
+            }
+            else {
+                ArrayList<String> dictArr = new ArrayList<>();
+                dictArr.addAll(dict);
+                ArrayList<TreeLevel> queue = new ArrayList<>();
+                queue.add(new TreeLevel(start, 1));
+                while (!queue.isEmpty()) {
+                    TreeLevel cur = queue.remove(0);
+                    int curLevel = cur.level;
+                    if (canStrTranslate(cur.val, end)) {
+                        return curLevel + 1;
+                    }
+                    else if (!dictArr.isEmpty()) {
+                        for (String item: dictArr) {
+
+                        }
+                        for (int i = 0; i < dictArr.size(); i++) {
+                            String item = dictArr.get(i);
+                            if (canStrTranslate(cur.val, item)) {
+                                queue.add(new TreeLevel(item, curLevel + 1));
+                                dictArr.remove(i);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private class TreeLevel {
+        public String val;
+        public int level;
+        public TreeLevel(String val, int level) {
+            this.level = level;
+            this.val = val;
+        }
+
+        @Override
+        public String toString() {
+            return "val=" + val + ",level=" + level;
+        }
+    }
+
+    private boolean canStrTranslate(String from, String to) {
+        int diffNum = 0;
+        for (int i = 0, len = from.length(); i < len; i++) {
+            if (from.charAt(i) != to.charAt(i)) {
+                diffNum += 1;
+                if (diffNum > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
+
+        /**
+         单词接龙   [中等]
+         给出两个单词（start和end）和一个字典，找到从start到end的最短转换序列
+         比如：
+         每次只能改变一个字母。
+         变换过程中的中间单词必须在字典中出现。
+         注意事项
+         如果没有转换序列则返回0。
+         所有单词具有相同的长度。
+         所有单词都只包含小写字母。
+
+         样例
+         给出数据如下：
+         start = "hit"
+         end = "cog"
+         dict = ["hot","dot","dog","lot","log"]
+         一个最短的变换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog"，
+         返回它的长度 5
+         */
+//        String start = "cet";
+//        String end = "ism";
+//        Set<String> dict = new HashSet<>();
+//        String[] dictArr = {"kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"};
+//        dict.addAll(Arrays.asList(dictArr));
+//        XYLog.d("利用词典", dict, "\n", start, " 转换到 ", end, " 的最少次数为 ");
+//        XYLog.d(solutions.ladderLength(start, end, dict));
+
+
+
+
 
         /**
          编辑距离   [中等]
