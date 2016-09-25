215cf092c7689808698e63d5583d9a419db9f551
dengbocao <847639468@qq.com>
2016-09-25 17:34:12 +0800

    重哈希   [中等]

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -8014,8 +8014,97 @@ public class Solutions {
     }
 
 
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/rehashing/
+     * @param hashTable: A list of The first node of linked list
+     * @return: A list of The first node of linked list which have twice size
+     */
+    public ListNode[] rehashing(ListNode[] hashTable) {
+        if (hashTable == null || hashTable.length == 0) {
+            return null;
+        }
+
+        int newLen = hashTable.length * 2;
+        ListNode[] newHashTable = new ListNode[newLen];
+        for (ListNode listNode: hashTable) {
+            ListNode temp = listNode;
+            while (temp != null) {
+                int hash = hash(temp.val, newLen);
+                if (newHashTable[hash] == null) {
+                    newHashTable[hash] = temp;
+                }
+                else {
+                    ListNode tail = newHashTable[hash];
+                    while (tail.next != null) {
+                        tail = tail.next;
+                    }
+                    tail.next = temp;
+                }
+                ListNode tempNext = temp.next;
+                temp.next = null;
+                temp = tempNext;
+            }
+        }
+        return newHashTable;
+    }
+
+
+    private int hash(int key, int size) {
+        if (key >= 0) {
+            return key % size;
+        }
+        else {
+            return (key % size + size) % size;
+        }
+    }
+
+
+
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
+
+        /**
+         重哈希   [中等]
+         哈希表容量的大小在一开始是不确定的。如果哈希表存储的元素太多（如超过容量的十分之一），我们应该将哈希表容量扩大一倍，并将所有的哈希值重新安排。假设你有如下一哈希表：
+         size=3, capacity=4
+         [null, 21, 14, null]
+               ↓    ↓
+                9   null
+               ↓
+              null
+         哈希函数为：
+         int hashcode(int key, int capacity) {
+         return key % capacity;
+         }
+         这里有三个数字9，14，21，其中21和9共享同一个位置因为它们有相同的哈希值1(21 % 4 = 9 % 4 = 1)。我们将它们存储在同一个链表中。
+         重建哈希表，将容量扩大一倍，我们将会得到：
+         size=3, capacity=8
+         index:   0    1    2    3     4    5    6   7
+         hash : [null, 9, null, null, null, 21, 14, null]
+         给定一个哈希表，返回重哈希后的哈希表。
+
+         注意事项
+
+         哈希表中负整数的下标位置可以通过下列方式计算：
+
+         C++/Java：如果你直接计算-4 % 3，你会得到-1，你可以应用函数：a % b = (a % b + b) % b得到一个非负整数。
+         Python：你可以直接用-1 % 3，你可以自动得到2。
+         您在真实的面试中是否遇到过这个题？ Yes
+         样例
+         给出 [null, 21->9->null, 14->null, null]
+         返回 [null, 9->null, null, null, null, 21->null, 14->null, null]
+         */
+//        ListNode[] hashTable = new ListNode[4];
+//        hashTable[1] = ListNodeFactory.build("21->9");
+//        hashTable[2] = ListNodeFactory.build("14");
+//        XYLog.d(hashTable, "\n扩容重排之后：");
+//        XYLog.d(solutions.rehashing(hashTable), "");
+
+
+
 
         /**
          哈希函数   [容易]
