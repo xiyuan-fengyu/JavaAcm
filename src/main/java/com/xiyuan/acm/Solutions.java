3f98342af4d7a81dbd2e24f4ecafc52d919cc94e
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-11 18:36:48 +0800

    数字组合 II  [中等]

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -9008,8 +9008,83 @@ public class Solutions {
     }
 
 
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/combination-sum-ii/
+     * @param arr: Given the candidate numbers
+     * @param target: Given the target number
+     * @return: All the combinations that sum to target
+     */
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        if (arr == null) {
+            return new ArrayList<>();
+        }
+        Arrays.sort(arr);
+        List<List<Integer>> result = findCombination(arr, target, 0);
+        HashMap<String, Boolean> existed = new HashMap<>();
+        for (int i = 0; i < result.size();) {
+            StringBuilder strBld = new StringBuilder();
+            List<Integer> item = result.get(i);
+            for (Integer j: item) {
+                strBld.append(j).append(',');
+            }
+            String key = strBld.toString();
+            if (existed.containsKey(key)) {
+                result.remove(i);
+            }
+            else {
+                existed.put(key, true);
+                i++;
+            }
+        }
+        return result;
+    }
+
+    private List<List<Integer>> findCombination(int[] arr, int target, int left) {
+        List<List<Integer>> result = new ArrayList<>();
+        int len = arr.length;
+        if (target != 0 && left < len && arr[left] <= target) {
+            List<List<Integer>> tempResult0 = findCombination(arr, target - arr[left], left + 1);
+            for (List<Integer> resultItem: tempResult0) {
+                resultItem.add(0, arr[left]);
+                result.add(resultItem);
+            }
+
+            List<List<Integer>> tempResult1 = findCombination(arr, target, left + 1);
+            if (tempResult1.size() > 0) {
+                result.addAll(tempResult1);
+            }
+        }
+        else if (target == 0){
+            result.add(new ArrayList<Integer>());
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
+
+        /**
+         数字组合 II  [中等]
+         给出一组候选数字(C)和目标数字(T),找出C中所有的组合，使组合中数字的和为T。C中每个数字在每个组合中只能使用一次。
+         注意事项
+         所有的数字(包括目标数字)均为正整数。
+         元素组合(a1, a2, … , ak)必须是非降序(ie, a1 ≤ a2 ≤ … ≤ ak)。
+         解集不能包含重复的组合。
+         您在真实的面试中是否遇到过这个题？ Yes
+         样例
+         给出一个例子，候选数字集合为[10,1,6,7,2,1,5] 和目标数字 8  ,
+         解集为：[[1,7],[1,2,5],[2,6],[1,1,6]]
+         */
+//        int[] arr = {10,1,6,7,2,1,5};
+//        int target = 8;
+//        XYLog.d(arr);
+//        XYLog.d("中所有和为 ", target, " 的组合有：\n", solutions.combinationSum2(arr, target));
+
 
         /**
          组合  [中等]
