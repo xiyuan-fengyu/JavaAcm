5ec4a5822dc17a6de7c2f55c61ce63e85499ae1d
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-12 13:54:10 +0800

    链表求和   [容易]

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -9614,8 +9614,59 @@ public class Solutions {
     }
 
 
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/add-two-numbers/
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2
+     */
+    public ListNode addLists(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode newHead = new ListNode(0);
+        ListNode cur = newHead;
+        while (l1 != null || l2 != null) {
+            int p1 = 0;
+            if (l1 != null) {
+                p1 = l1.val;
+                l1 = l1.next;
+            }
+
+            int p2 = 0;
+            if (l2 != null) {
+                p2 = l2.val;
+                l2 = l2.next;
+            }
+
+            int sum = p1 + p2 + carry;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            carry = sum / 10;
+        }
+
+        if (carry > 0) {
+            cur.next = new ListNode(1);
+        }
+        return newHead.next;
+    }
+
+
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
+
+        /**
+         链表求和   [容易]
+         你有两个用链表代表的整数，其中每个节点包含一个数字。数字存储按照在原来整数中相反的顺序，使得第一个数字位于链表的开头。写出一个函数将两个整数相加，用链表形式返回和。
+         样例
+         给出两个链表 3->1->5->null 和 5->9->2->null，返回 8->0->8->null
+         */
+//        ListNode list1 = ListNodeFactory.build("3->1");
+//        ListNode list2 = ListNodeFactory.build("5->9");
+//        XYLog.d(list1, " + ", list2, " = ", solutions.addLists(list1, list2));
+
+
 
         /**
          链表倒数第n个节点   [容易]
