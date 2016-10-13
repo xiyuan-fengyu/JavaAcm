1e2d2b4c5accc95b83971c5211dfb859a2fa3ac1
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-13 19:05:41 +0800

    删除数字   [中等]

--- src/main/java/com/xiyuan/acm/Solutions.java
+++ src/main/java/com/xiyuan/acm/Solutions.java
@@ -10324,12 +10324,40 @@ public class Solutions {
     }
 
 
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/delete-digits/
+     * @param arr: A positive integer which has N digits, A is a string.
+     * @param k: Remove k digits.
+     * @return: A string
+     */
+    public String DeleteDigits(String arr, int k) {
+
+        return null;
+    }
+
+
     public static void main(String[] args) {
         Solutions solutions = new Solutions();
 
         /**
          删除数字   [中等]
+         给出一个字符串 A, 表示一个 n 位正整数, 删除其中 k 位数字, 使得剩余的数字仍然按照原来的顺序排列产生一个新的正整数。
+         找到删除 k 个数字之后的最小正整数。
+         N <= 240, k <= N
+         样例
+         给出一个字符串代表的正整数 A 和一个整数 k, 其中 A = 178542, k = 4
+         返回一个字符串 "12"
          */
+        String str = "178542";
+        int k = 4;
+        XYLog.d(str, "中删除", k, "个数字能得到的最小正整数为：", solutions.DeleteDigits(str, k));
+
+
+
 
 
         /**
