1ecf6124ea1f271a719843e7c07a79d9ecdd39e0
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-14 14:20:22 +0800

    最大数   [中等]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -1,6 +1,7 @@
 package com.xiyuan.acm;
 
 import com.xiyuan.acm.model.ExpressionTreeNode;
+import com.xiyuan.util.XYLog;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -390,8 +391,37 @@ public class Solutions184_564 {
 
 
 
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/largest-number/
+     * @param nums: A list of non negative integers
+     * @return: A string
+     */
+    public String largestNumber(int[] nums) {
+
+        return null;
+    }
+
     public static void main(String[] args) {
         Solutions184_564 solutions = new Solutions184_564();
+
+        /**
+         最大数   [中等]
+         http://www.lintcode.com/zh-cn/problem/largest-number/
+         给出一组非负整数，重新排列他们的顺序把他们组成一个最大的整数。
+         注意事项
+         最后的结果可能很大，所以我们返回一个字符串来代替这个整数。
+         样例
+         给出 [1, 20, 23, 4, 8]，返回组合最大的整数应为8423201。
+         挑战
+         在 O(nlogn) 的时间复杂度内完成。
+         */
+        int[] nums = {1, 20, 23, 4, 8};
+        XYLog.d(nums, "能拼接成的最大整数为：", solutions.largestNumber(nums));
 
 
 
