05eafde1c0eded5172465debbe9de4ce68b2c920
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-21 14:10:47 +0800

    单例   [容易]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -1148,8 +1148,57 @@ public class Solutions184_564 {
         }
     }
 
+
+
+
+
+
+
+
+
+    private static class Solution {
+
+        private static Solution instance = new Solution();
+
+        private Solution() {}
+
+        /**
+         * http://www.lintcode.com/zh-cn/problem/singleton/
+         * @return: The same instance of this class every time
+         */
+        public static Solution getInstance() {
+            return instance;
+        }
+    }
+
     public static void main(String[] args) {
         Solutions184_564 solutions = new Solutions184_564();
+
+        /**
+         单例   [容易]
+         http://www.lintcode.com/zh-cn/problem/singleton/
+         单例 是最为最常见的设计模式之一。对于任何时刻，如果某个类只存在且最多存在一个具体的实例，那么我们称这种设计模式为单例。例如，对于 class Mouse (不是动物的mouse哦)，我们应将其设计为 singleton 模式。
+         你的任务是设计一个 getInstance 方法，对于给定的类，每次调用 getInstance 时，都可得到同一个实例。
+         样例
+         在 Java 中:
+         A a = A.getInstance();
+         A b = A.getInstance();
+         a 应等于 b.
+         挑战
+         如果并发的调用 getInstance，你的程序也可以正确的执行么？
+         */
+//        for (int i = 0; i < 1000; i++) {
+//            new Thread() {
+//                @Override
+//                public void run() {
+//                    XYLog.d(Solution.getInstance().hashCode());
+//                }
+//            }.start();
+//        }
+
+
+
+
 
         /**
          线段树的修改   [中等]
