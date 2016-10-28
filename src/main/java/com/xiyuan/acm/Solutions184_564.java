d45e527c1eb9c74a838e88e495cb6a23ec5569ea
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-28 17:19:16 +0800

    报数   [中等]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -3518,8 +3518,45 @@ public class Solutions184_564 {
         return result;
     }
 
+
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/count-and-say/
+     * @param n the nth
+     * @return the nth sequence
+     */
+    public String countAndSay(int n) {
+
+        return "";
+    }
+
     public static void main(String[] args) {
         Solutions184_564 solutions = new Solutions184_564();
+
+        /**
+         报数   [中等]
+         http://www.lintcode.com/zh-cn/problem/count-and-say/
+         报数指的是，按照其中的整数的顺序进行报数，然后得到下一个数。如下所示：
+         1, 11, 21, 1211, 111221, ...
+         1 读作 "one 1" -> 11.
+         11 读作 "two 1s" -> 21.
+         21 读作 "one 2, then one 1" -> 1211.
+         给定一个整数 n, 返回 第 n 个顺序。
+         注意事项
+         整数的顺序将表示为一个字符串。
+         样例
+         给定 n = 5, 返回 "111221".
+         */
+        int n = 5;
+        XYLog.d(solutions.countAndSay(n));
+
+
+
+
 
         /**
          罗马数字转整数   [中等]
