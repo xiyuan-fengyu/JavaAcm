210efd0d095278e39698103b824181f4abbea911
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-28 16:45:54 +0800

    有效数字   [困难]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -3378,46 +3378,6 @@ public class Solutions184_564 {
 
 
 
-    /**
-     * http://www.lintcode.com/zh-cn/problem/valid-number/
-     * @param s the string that represents a number
-     * @return whether the string is a valid number
-     */
-    public boolean isNumberTest(String s) {
-        int len = s.length();
-        int i = 0, e = len - 1;
-        while (i <= e && Character.isWhitespace(s.charAt(i))) i++;
-        if (i > len - 1) return false;
-        while (e >= i && Character.isWhitespace(s.charAt(e))) e--;
-        // skip leading +/-
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') i++;
-        boolean num = false; // is a digit
-        boolean dot = false; // is a '.'
-        boolean exp = false; // is a 'e'
-        while (i <= e) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                num = true;
-            }
-            else if (c == '.') {
-                if(exp || dot) return false;
-                dot = true;
-            }
-            else if (c == 'e') {
-                if(exp || num == false) return false;
-                exp = true;
-                num = false;
-            }
-            else if (c == '+' || c == '-') {
-                if (s.charAt(i - 1) != 'e') return false;
-            }
-            else {
-                return false;
-            }
-            i++;
-        }
-        return num;
-    }
 
     /**
      * http://www.lintcode.com/zh-cn/problem/valid-number/
@@ -3485,9 +3445,7 @@ public class Solutions184_564 {
                     return false;
                 }
             }
-            else if (c >= '0' && c <= '9') {
-            }
-            else {
+            else if (c < '0' || c > '9') {
                 return false;
             }
         }
