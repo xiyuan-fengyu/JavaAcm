c144a9f714de694c59e24714be3c6d4fe8ac2365
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-30 17:03:13 +0800

    x的n次幂   [中等]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -3879,8 +3879,63 @@ public class Solutions184_564 {
     }
 
 
+
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/powx-n/
+     * @param x the base number
+     * @param n the power number
+     * @return the result
+     */
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else if (n == -1) {
+            return 1 / x;
+        }
+        else if (x == 0) {
+            return n > 0? 0 : 1 / x;
+        }
+        else {
+            double temp = myPow(x, n / 2);
+            if (n % 2 == 0) {
+                return temp * temp;
+            }
+            else if (n > 0) {
+                return temp * temp * x;
+            }
+            else {
+                return temp * temp * 1 / x;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Solutions184_564 solutions = new Solutions184_564();
+
+        /**
+         x的n次幂   [中等]
+         http://www.lintcode.com/zh-cn/problem/powx-n/
+         实现 pow(x,n)
+         注意事项
+         不用担心精度，当答案和标准输出差绝对值小于1e-3时都算正确
+         样例
+         Pow(2.1, 3) = 9.261
+         Pow(0, 1) = 0
+         Pow(1, 0) = 1
+         */
+//        double x = 0;
+//        int n = -4;
+//        XYLog.d(solutions.myPow(x, n));
+
+
+
 
         /**
          生成括号   [中等]
