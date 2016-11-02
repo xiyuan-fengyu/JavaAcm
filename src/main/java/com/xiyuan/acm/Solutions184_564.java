0c916cfe53c92ba703efa15b71657235b655389f
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-11-02 17:32:38 +0800

    快乐数   [容易]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -4600,8 +4600,56 @@ public class Solutions184_564 {
         }
     }
 
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/happy-number/
+     * @param n an integer
+     * @return true if this is a happy number or false
+     */
+    public boolean isHappy(int n) {
+        HashMap<Long, Boolean> cache = new HashMap<>();
+        long temp = n;
+        while (!cache.containsKey(temp = happChange(temp)) && temp != 1) {
+            cache.put(temp, true);
+        }
+        return temp == 1;
+    }
+
+    private long happChange(long n) {
+        long total = 0;
+        while (n > 0) {
+            long temp = n % 10;
+            total += temp * temp;
+            n /= 10;
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
         Solutions184_564 solutions = new Solutions184_564();
+
+        /**
+         快乐数   [容易]
+         http://www.lintcode.com/zh-cn/problem/happy-number/
+         写一个算法来判断一个数是不是"快乐数"。
+         一个数是不是快乐是这么定义的：对于一个正整数，每一次将该数替换为他每个位置上的数字的平方和，
+         然后重复这个过程直到这个数变为1，或是无限循环但始终变不到1。如果可以变为1，那么这个数就是快乐数。
+         样例
+         19 就是一个快乐数。
+         1^2 + 9^2 = 82
+         8^2 + 2^2 = 68
+         6^2 + 8^2 = 100
+         1^2 + 0^2 + 0^2 = 1
+         */
+//        XYLog.d(solutions.isHappy(18));
+
+
+
+
 
         /**
          二叉树的所有路径   [容易]
