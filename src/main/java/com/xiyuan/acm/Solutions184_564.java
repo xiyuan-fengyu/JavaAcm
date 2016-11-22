a9a2aa6e1dd4b13fad6dc21608ba1026130873f9
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-11-22 17:34:26 +0800

    丑数   [容易]

--- src/main/java/com/xiyuan/acm/Solutions184_564.java
+++ src/main/java/com/xiyuan/acm/Solutions184_564.java
@@ -5156,8 +5156,53 @@ public class Solutions184_564 {
         return min;
     }
 
+
+
+
+
+
+    /**
+     * http://www.lintcode.com/zh-cn/problem/ugly-number/
+     * @param num an integer
+     * @return true if num is an ugly number or false
+     */
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        else if (num == 1) {
+            return true;
+        }
+        else if (num % 2 == 0) {
+            return isUgly(num / 2);
+        }
+        else if (num % 3 == 0) {
+            return isUgly(num / 3);
+        }
+        else if (num % 5 == 0) {
+            return isUgly(num / 5);
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         Solutions184_564 solutions = new Solutions184_564();
+
+        /**
+         丑数   [容易]
+         http://www.lintcode.com/zh-cn/problem/ugly-number/
+         写一个程序来检测一个整数是不是丑数。
+         丑数的定义是，只包含质因子 2, 3, 5 的正整数。比如 6, 8 就是丑数，但是 14 不是丑数以为他包含了质因子 7。
+         注意事项
+         可以认为 1 是一个特殊的丑数。
+         */
+        int n = 14;
+        XYLog.d(solutions.isUgly(n));
+
+
+
+
 
         /**
          房屋染色II   [困难]
